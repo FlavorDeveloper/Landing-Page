9f6d445072891e416bc59810d432b73afbaa83c4
Flavorino <161358985+FlavorDeveloper@users.noreply.github.com>
2025-03-13 11:49:08 -0400

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+// Importaciones.
+import { animation } from "./modules/animation.js";
+import { validarForm } from "./modules/validarform.js";
+// Modulos.
+document.addEventListener('DOMContentLoaded', () => {
+    animation();
+    validarForm();
+})
