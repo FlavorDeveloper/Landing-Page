18b7792f00b700a6b5fa00717a17f232e7becc51
Flavorino <161358985+FlavorDeveloper@users.noreply.github.com>
2025-03-13 13:02:58 -0400

    Delete JS directory

--- JS/app.js
+++
@@ -1,8 +0,0 @@
-// Importaciones.
-import { animation } from "./modules/animation.js";
-import { validarForm } from "./modules/validarform.js";
-// Modulos.
-document.addEventListener('DOMContentLoaded', () => {
-    animation();
-    validarForm();
-})
